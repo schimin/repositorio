a658b3e8f36b049780e1b9981f0009e8947e69cd
schimin <robertojoaojr@gmail.com>
2022-11-28 09:25:07 -0300

    Create check_key_value_array.js

---
+++ javascript/check_key_value_array.js
@@ -0,0 +1,13 @@
+// QUANDO FOR CHAVE
+for (const [key, value] of Object.entries(cesta)) {
+  if (key.includes('quantidade')) {
+    // 'quantidade' é uma chave dentro do array
+  }
+}
+
+
+// QUANDO FOR VALOR
+const lista = [{}]; // sua lista 
+const produto  = lista.find(item => item.id == 6)
+
+(produto) ? produto : 'Nada encontrado';
