f3a4bb3b9feccba6d38287738c65495dcd77f924
schimin <robertojoaojr@gmail.com>
2023-05-12 15:02:56 -0300

    Create proteger_requisicao_direta.php

---
+++ php/proteger_requisicao_direta.php
@@ -0,0 +1,3 @@
+define('AJAX_REQUEST', isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest');
+if(!AJAX_REQUEST) {die('Direct Access not allowed.');}
+// PROTEGE BACKEND DE ACESSO DIRETO
